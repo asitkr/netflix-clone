8645363de4e33ed5e6c94f776a6f58812d9ea86b
asitkr <krsinhasaurabh@gmail.com>
2022-10-17 23:11:52 +0530

    partial completed Banner Component

--- src/Request.jsx
+++ src/Request.jsx
@@ -4,7 +4,7 @@ const API_Key = "516526ef54f1959645a3b554dcaad522";
 const requests = {
     fetchTrending: `/trending/all/week?api_key=${API_Key}&language=en-US`,
     fetchNetflixOriginals: `/discover/tv?api_key=${API_Key}&with_networks=213`,
-    fetchTopRated: `/movies/top_rated?api_key=${API_Key}&language=en-US`,
+    fetchTopRated: `/movie/top_rated?api_key=${API_Key}&language=en-US`,
     fetchActionMovies: `/discover/movie?api_key=${API_Key}&with_genres=28`,
     fetchComedyMovies: `/discover/movie?api_key=${API_Key}&with_genres=35`,
     fetchHorrorMovies: `/discover/movie?api_key=${API_Key}&with_genres=27`,
---
+++ src/components/Row.jsx
@@ -0,0 +1,43 @@
+import axios from '../Axios';
+import React, { useEffect, useState } from 'react';
+import './Row.css';
+
+const Row = ({ title, fetchURL, isLargeRow = false }) => {
+    
+    const [movies, setMovies] = useState([]);
+
+    const baseURL = "https://image.tmdb.org/t/p/original/";
+
+    useEffect(() => {
+        async function fetchData() {
+            const request = await axios.get(fetchURL);
+            setMovies(request.data.results);
+            return request;
+        }
+
+        fetchData();
+    }, [fetchURL])
+
+    console.log(movies);
+
+    return (
+        <div className='row'>
+            <h2>{title}</h2>
+
+            <div className='row__posters'>
+                {
+                    movies.map(movie => (
+                        <img
+                            key={movie.id}
+                            className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+                            src={`${baseURL}${
+                                isLargeRow ? movie.poster_path : movie.backdrop_path
+                        }`} alt={movie.name} />
+                    ))
+                }
+            </div>
+        </div>
+    )
+}
+
+export default Row;
